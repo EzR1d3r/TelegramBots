e3fdd22a41a23e58cdec05858dd9423cd2a46922
EzR1d3r <brd1080@yandex.ru>
2019-06-29 23:25:50 +0300

    эксперименты с flask

--- echo_bot.py
+++ echo_bot.py
@@ -3,11 +3,24 @@
 
 from bots.EchoBot import EchoBot
 from lib.BotProcessor import BotProcessor
+from flask import Flask, request
+
+app = Flask( __name__ )
+resp = "<h1>Hello</h1>"
+
+@app.route("/", methods=["POST", "GET"])
+def index():
+    if request.method == "POST":
+        r = request.get_json()
+        print(r, type(r))
+    return resp
+
 
 def main():
-    handler = EchoBot()
-    processor = BotProcessor(handler)
-    processor.process()
+    app.run()
+    # handler = EchoBot()
+    # processor = BotProcessor(handler)
+    # processor.process()
 
 if __name__ == '__main__':  
     try:
