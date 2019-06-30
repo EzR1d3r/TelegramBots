96234e7c198191e5dcfb3837da5762f9b2050bb0
EzR1d3r <brd1080@yandex.ru>
2019-06-30 14:14:38 +0300

    openssl

--- echo_bot.py
+++ echo_bot.py
@@ -4,10 +4,15 @@
 from bots.EchoBot import EchoBot
 from lib.BotProcessor import BotProcessor
 from flask import Flask, request
-from flask_sslify import SSLify
+# from flask_sslify import SSLify
+
+from OpenSSL import SSL
+context = SSL.Context(SSL.SSLv23_METHOD)
+context.use_privatekey_file('yourserver.key')
+context.use_certificate_file('yourserver.crt')
 
 app = Flask( __name__ )
-sslify = SSLify(app)
+# sslify = SSLify(app)
 
 # app.debug = False
 resp = "<h1>Hello</h1>"
@@ -21,7 +26,7 @@ def index():
 
 
 def main():
-    app.run(host="0.0.0.0", port="443")
+    app.run(host="0.0.0.0", port="443", ssl_context=context)
     # handler = EchoBot()
     # processor = BotProcessor(handler)
     # processor.process()
